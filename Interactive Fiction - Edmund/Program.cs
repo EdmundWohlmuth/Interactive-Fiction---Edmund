7777b76c87a3f3bb130d2e92e2c1b4e593c06bad
Edmund Wohlmuth <edmundwoh@gmail.com>
2021-11-25 15:18:21 -0400

    Moved intro text to Story[0]

--- Interactive Fiction - Edmund/Program.cs
+++ Interactive Fiction - Edmund/Program.cs
@@ -10,7 +10,7 @@ namespace Interactive_Fiction___Edmund
     {
         //decleration
 
-        static int storyLength = 15;
+        static int storyLength = 16;
         static int pageNum; // element that determins page to be displayed
 
         static string[] story = new string[storyLength]; // Stores the story / title & pages
@@ -30,8 +30,7 @@ namespace Interactive_Fiction___Edmund
             pageNum = 0;
             isPlaying = false;
 
-            MainMenu();
-            EstablishingText();        
+            MainMenu();      
 
                 //gameplay loop
                 while (isGameOver == false && isPlaying) //gameloop (while not game over and is past Main Menu
@@ -45,24 +44,24 @@ namespace Interactive_Fiction___Edmund
         }
 
         static void PlotText()
-        {                     
+        {
             // Story Text blocks
-            story[0] = "The large fire-breathing wyvern bears upon you!;1: Parry and retreat;2: Roll to the left;1;3";
-            story[1] = "You swing your sword in an arch, fire singeing your armor;1: Heavy overhead swing at the Rathian;2: Riposte and sidestep;2;3";
-            story[2] = "You prepare a heavy swing, but the Rathian is faster, a fireball strikes you in the chest and you're on the ground!;1: Get up!;2: Roll left!;4;6";
-            story[3] = "Your evasive moves avoid a dangerous strike, and you prepare a heavy hit in return;1: Strike the head;2: Go for the legs;2;5";
-            story[4] = "The time it takes for you too get up is not fast enough, as the Rathian's toothy maw closes down on you.;You've Died;Press 2 to restart;4;4"; // death 1
-            story[5] = "You swing for the legs knocking it down!;1: Follow up with a strike to its head;2: Climb the Rathian to move to its blind spot;2;7";
-            story[6] = "You roll to the side as the rathian stomps down on where you used to be, standing now you make your move;1: Stand your ground;2: Take a swing at the Rathian;0;1";
-            story[7] = "You've climbed to the Rathian's back as it rights its self and begins to fly away, with you on its back!;1: Jump off before its too late;2: Hang on!;13;8";
-            story[8] = "You hunker down and hang on as the Rathian flys higher and higher;1: Jump off;2: Hang on;13;9";
-            story[9] = "The Rathian reaches its crusing altitude, and you are comfortable enough to manuever;1: Climb to the Head;2: Climb to the wings;10;11";
-            story[10] = "You've reached the Head of the Rathian, despite its attempts to shake you off you hang on, you could reach for your main weapon, as it would ensure a kill, but is sacrificing your stability worth it?;1: Reach for your main weapon;2: Reach for your knife;12;14";
-            story[11] = "The area around the wings is alot harder to keep your grip on, but if you could just get a little closer, you might be able to bring the beat down...;1: Continue to the wings;2:Go to the head instead;12;10";
-            story[12] = "The flight becomes too much and your grip on the Rathian releases, its a long, long fall to the ground, and you think there's no way to walk away from this as the ground meets your face and sudden darkness envelopes your world.;You've died!;2: Quit;14;12"; // death 2
-            story[13] = "You jump off the Rathian, as it flys up high and away. The End.;1: Restart;2: Quit;14;13"; // win 1
-            story[14] = "With your knife drawn you stab the Rathian in the eye, and using its body as a brace you survive the landing, resulting in an unexpected hunt to be sure, but a welcome one. The End.;1: Restart;2: Quit;14;14"; // win 2
-
+            story[0] = "You are a Monster Hunter, and on a routine hunt you've been ambushed by a Rathian!;Press 1 or 2 to progress the story;Press 3 to save, and 4 to Quit;1;1";
+            story[1] = "The large fire-breathing wyvern bears upon you!;1: Parry and retreat;2: Roll to the left;2;4";
+            story[2] = "You swing your sword in an arch, fire singeing your armor;1: Heavy overhead swing at the Rathian;2: Riposte and sidestep;3;4";
+            story[3] = "You prepare a heavy swing, but the Rathian is faster, a fireball strikes you in the chest and you're on the ground!;1: Get up!;2: Roll left!;5;7";
+            story[4] = "Your evasive moves avoid a dangerous strike, and you prepare a heavy hit in return;1: Strike the head;2: Go for the legs;3;6";
+            story[5] = "The time it takes for you too get up is not fast enough, as the Rathian's toothy maw closes down on you.;You've Died;Press 2 to restart;5;5"; // death 1
+            story[6] = "You swing for the legs knocking it down!;1: Follow up with a strike to its head;2: Climb the Rathian to move to its blind spot;3;8";
+            story[7] = "You roll to the side as the rathian stomps down on where you used to be, standing now you make your move;1: Stand your ground;2: Take a swing at the Rathian;1;2";
+            story[8] = "You've climbed to the Rathian's back as it rights its self and begins to fly away, with you on its back!;1: Jump off before its too late;2: Hang on!;14;9";
+            story[9] = "You hunker down and hang on as the Rathian flys higher and higher;1: Jump off;2: Hang on;14;10";
+            story[10] = "The Rathian reaches its crusing altitude, and you are comfortable enough to manuever;1: Climb to the Head;2: Climb to the wings;11;12";
+            story[11] = "You've reached the Head of the Rathian, despite its attempts to shake you off you hang on, you could reach for your main weapon, as it would ensure a kill, but is sacrificing your stability worth it?;1: Reach for your main weapon;2: Reach for your knife;13;15";
+            story[12] = "The area around the wings is alot harder to keep your grip on, but if you could just get a little closer, you might be able to bring the beat down...;1: Continue to the wings;2:Go to the head instead;13;11";
+            story[13] = "The flight becomes too much and your grip on the Rathian releases, its a long, long fall to the ground, and you think there's no way to walk away from this as the ground meets your face and sudden darkness envelopes your world.;You've died!;2: Quit;15;13"; // death 2
+            story[14] = "You jump off the Rathian, as it flys up high and away. The End.;1: Restart;2: Quit;15;14"; // win 1
+            story[15] = "With your knife drawn you stab the Rathian in the eye, and using its body as a brace you survive the landing, resulting in an unexpected hunt to be sure, but a welcome one. The End.;1: Restart;2: Quit;15;15"; // win 2
 
         } 
         
@@ -97,12 +96,12 @@ namespace Interactive_Fiction___Edmund
                     break;
 
                 case "3":
-                    Console.WriteLine("Temp text, would normally save game");
+                    Console.WriteLine("Temp text would save game");
                     UserChoice();
                     break;
 
                 case "4":
-                    isGameOver = false;
+                    Environment.Exit(0);
                     break;
 
                 default: // basically the else statement
@@ -127,22 +126,20 @@ namespace Interactive_Fiction___Edmund
         {
             //  insert ASCII text title screen here
 
-            Console.WriteLine("Title Screen!");
+            Console.WriteLine("Monster Hunter! - An interactive story.");
             Console.WriteLine("==============");
             Console.WriteLine("Press 1 to start\nPress 2 to continue\nPress 3 to exit");
+            Console.WriteLine(" ");
+            Console.WriteLine("By: Edmund Wohlmuth");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("'Monster Hunter' is a game series developed and published by CAPCOM");
+            Console.ForegroundColor = ConsoleColor.White;
+            
 
             MenuSelect();
 
-        }
-
-        static void EstablishingText()
-        {
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("You are a Monster Hunter, and on a routine hunt you've been ambushed by a Rathian!");
-            Console.WriteLine("Press any key to begin");
-            Console.WriteLine(" ");
-            Console.ReadKey();
-            Console.Clear();
         }
 
         static void MenuSelect()
@@ -162,7 +159,7 @@ namespace Interactive_Fiction___Edmund
                     break;
 
                 case "3":
-                    Console.WriteLine("Temp text, will eventually exit the program");
+                    Environment.Exit(0);
                     break;
 
                 default:
